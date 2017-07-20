a602eaa32c5a362c855be17e19cc5f69655b9b1d
rallristhy <rallristhy@outlook.com>
2017-07-20 17:52:27 -0400

    Adaptação para ler o json list com varios estabelecimentos...

--- TrabalhoAndroid/app/src/main/java/com/rallristhy/trabalho/trabalhoandroid/MapsActivity.java
+++ TrabalhoAndroid/app/src/main/java/com/rallristhy/trabalho/trabalhoandroid/MapsActivity.java
@@ -36,26 +36,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     public void recebeJson(String s){
         Gson gson = new Gson();
-        //Log.d("R4LL", s);
-        //Estabelecimentos e = gson.fromJson(s, Estabelecimentos.class);
 
         Estabelecimentos[] estabelecimentoses = gson.fromJson(s, Estabelecimentos[].class);
 
         for (int i = 0; i < estabelecimentoses.length; i++) {
             Log.d("R4LL", "Nome: "+estabelecimentoses[i].getNome());
-            Log.d("R4LL", "Nome: "+estabelecimentoses[i].getCoord().getLat());
-            Log.d("R4LL", "Nome: "+estabelecimentoses[i].getCoord().getLon());
+            Log.d("R4LL", "Latitude: "+estabelecimentoses[i].getCoord().getLat());
+            Log.d("R4LL", "Longitude: "+estabelecimentoses[i].getCoord().getLon());
+            Log.d("R4LL", "");
 
         }
-        //Log.d("R4LL", estabelecimentoses[0].getNome()+"");
-
-        //Log.d("R4LL", estabelecimentos[0].getNome());
-        //Log.d("R4LL", estabelecimentos[0].getCoord().getLat()+"");
-        //Log.d("R4LL", estabelecimentos[0].getCoord().getLon()+"");
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng tvmorena = new LatLng(-20.479415, -54.600827);
+        LatLng padaria = new LatLng(-20.478790, -54.599307);
+        mMap.addMarker(new MarkerOptions().position(padaria).title("Pão Moreno"));
+        mMap.addMarker(new MarkerOptions().position(tvmorena).title("TV Morena"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tvmorena));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(padaria));
+    }
+
+    public void onMapReady(GoogleMap googleMap, String s) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
